fd1d3860df550909a6993784f6963216394ebe76
Einbert-Xeride <einbert-xeride@outlook.com>
2018-06-23 19:46:36 +0800

    Removed GNU utils aliases

--- zsh/zsh-custom/core/81-macos-gnu-sed.zsh
+++
@@ -1,5 +0,0 @@
-if isdarwin && type gsed NUL; then
-    alias bsd-sed='/usr/bin/sed'
-
-    alias sed='/usr/local/bin/gsed'
-fi
--- zsh/zsh-custom/core/81-macos-gnu-tar.zsh
+++
@@ -1,5 +0,0 @@
-if isdarwin && type gtar NUL; then
-    alias bsd-tar='/usr/bin/tar'
-
-    alias tar='/usr/local/bin/gtar'
-fi
